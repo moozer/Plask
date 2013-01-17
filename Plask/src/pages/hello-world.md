0fc34d0cbc44fe026074186ef887510d07ab98c5
moozer <bigmoozer@gmail.com>
2013-01-17 16:06:06 +0100

    first commit

---
+++ Plask/src/pages/hello-world.md
@@ -0,0 +1,8 @@
+title: Hello World
+date: 2012-03-04
+tags: [general, awesome, stuff]
+author: Morten Bo Nielsen
+email: mon@eal.dk
+
+**Hello World**, from a *page*!
+
---
+++ Plask/src/sitebuilder.py
@@ -0,0 +1,33 @@
+'''
+Created on 17 Jan 2013
+
+@author: moz
+'''
+
+from flask import Flask, render_template
+from flask_flatpages import FlatPages
+
+DEBUG = True
+FLATPAGES_AUTO_RELOAD = DEBUG
+FLATPAGES_EXTENSION = '.md'
+
+app = Flask(__name__)
+app.config.from_object(__name__)
+pages = FlatPages(app)
+
+@app.route('/')
+def index():
+    return render_template('index.html', pages=pages)
+
+@app.route('/<path:path>/')
+def page(path):
+    page = pages.get_or_404(path)
+    return render_template('page.html', page=page)
+
+@app.route('/tag/<string:tag>/')
+def tag(tag):
+    tagged = [p for p in pages if tag in p.meta.get('tags', [])]
+    return render_template('tag.html', pages=tagged, tag=tag)
+
+if __name__ == '__main__':
+    app.run(port=8000)
