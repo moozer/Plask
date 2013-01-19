3f869ad64d055a7b8309104d9d1e79a961800925
moozer <bigmoozer@gmail.com>
2013-01-19 01:58:04 +0100

    added listpages or course descriptoins.

--- Plask/src/sitebuilder.py
+++ Plask/src/sitebuilder.py
@@ -19,6 +19,17 @@ app.config.from_object(__name__)
 pages = FlatPages(app)
 freezer = Freezer(app)
 
+def getCourses( semester = None ):
+    if not semester:
+        return None
+    
+    Courses = [ c for c in os.listdir("pages/%s"%semester) if os.path.isdir("pages/%s/%s"%(semester,c))]
+    return Courses
+
+def getSemesters():
+    semesters = [ c for c in os.listdir("pages/") if os.path.isdir("pages/%s"%c)]
+    return semesters
+
 @app.route('/')
 def index():
     return render_template('index.html', pages=pages)
@@ -28,8 +39,16 @@ def tag(tag):
     tagged = [p for p in pages if tag in p.meta.get('tags', [])]
     return render_template('tag.html', pages=tagged, tag=tag)
 
+@app.route('/fagplan/')
+@app.route('/fagplan/<string:semester>/')
 @app.route('/fagplan/<string:semester>/<string:course>')
-def fagplan(course, semester):
+def fagplan(course = None, semester = None):
+    if not course or not semester:
+        return render_template('fagplanindex.html', 
+                        semesters=getSemesters(), semester=semester, 
+                        courses=getCourses( semester ))
+
+    
     dirname = u"%s/%s"%(semester,course) 
     if not os.path.isdir("pages/"+dirname):
         abort( 404 )
@@ -56,6 +75,7 @@ if __name__ == '__main__':
 #        print pages
 #        basepages = [p for p in pages if dirname == os.path.dirname(p.path)]
 #        print basepages
-        #fagplan("ITT2 Networking", "2013S")
+#        fagplan("ITT2 Networking", "2013S")
+        #fagplan()
         app.run(port=8000)
         
