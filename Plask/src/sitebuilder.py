8ba72ce02a3724a601f7843be926da7a4053935f
moozer <bigmoozer@gmail.com>
2013-01-21 14:09:37 +0100

    Added overview which collects across courses :-)

--- Plask/src/sitebuilder.py
+++ Plask/src/sitebuilder.py
@@ -10,6 +10,8 @@ import sys, os
 from flask_frozen import Freezer
 import csv
 
+coursesections = ['Introduction', 'Teaching goals', 'Learning goals', 'Evaluation', 'Literature', 'Exam questions']
+
 DEBUG = True
 FLATPAGES_AUTO_RELOAD = DEBUG
 FLATPAGES_EXTENSION = '.md'
@@ -49,7 +51,6 @@ def fagplan(course = None, semester = None):
         return render_template('fagplanindex.html', 
                         semesters=getSemesters(), semester=semester, 
                         courses=getCourses( semester ))
-
     
     dirname = u"%s/%s"%(semester,course) 
     if not os.path.isdir("pages/"+dirname):
@@ -61,6 +62,19 @@ def fagplan(course = None, semester = None):
     reader = csv.DictReader(open("pages/"+dirname+"/schedule.csv", 'r'), delimiter='\t')
     schedule = [ entry for entry in reader]
     return render_template('fagplan.html', schedule=schedule, course=course, semester=semester, pages=basepages)
+
+@app.route('/overview/')
+@app.route('/overview/<string:semester>/')
+@app.route('/overview/<string:semester>/<string:overview>/')
+@app.route('/overview/<string:semester>/<string:overview>.html')
+def overview(overview = None, semester = None):
+    if not overview or not semester:
+        return render_template('overviewindex.html', 
+                        semesters=getSemesters(), semester=semester, 
+                        coursesections=coursesections)
+
+    basepages = [p for p in pages if overview in p.meta.get('sectionname', []) and semester in p.path ]
+    return render_template('overview.html', semester=semester, pages=basepages, overview=overview)
 
 @app.route('/<path:path>/')
 def page(path):
@@ -79,5 +93,6 @@ if __name__ == '__main__':
 #        print basepages
 #        fagplan("ITT2 Networking", "2013S")
         #fagplan()
+        #overview( semester="2013S", overview="Evaluation" )
         app.run(port=8000)
         
