f2bc3bf5224a8b565032aed5b9c9edf840b16c08
moz <moz@local>
2013-06-30 19:01:59 +0200

    clean up

--- Plask/src/tests/testFiles.py
+++ Plask/src/tests/testFiles.py
@@ -10,35 +10,6 @@ DataDir = "../testData"
 TestSemesters = ["Sem_A"]
 TestCourses1 = ['Course_A', 'Course_B']
 TestLinks = ['Link_A', 'Link_B']
-#TestCourses =
-
-# class FpMock:
-#     class configClass:
-#         
-#         data = { 'FLATPAGES_AUTO_RELOAD': True, 'FLATPAGES_EXTENSION': '.md', 
-#                 'FLATPAGES_ROOT': '.' }
-#         
-#         def setdefault(self, key, val):
-#             if key in self.data.keys():
-#                 return
-#             self.data[key] = val
-#             print "key: %s, val:%s"%(key,self.data[key] )
-#             return
-#     
-#         def __getitem__(self, key):
-#             return self.data[key]
-#         
-#     @property
-#     def config(self):
-#         return self.configClass()
-# 
-#     def before_request( self, conditional_auto_reset):
-#         return DataDir
-#     
-#     @property 
-#     def root_path(self):
-#         return DataDir
-
 
 class Test(unittest.TestCase):
 
